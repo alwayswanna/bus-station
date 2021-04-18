7ce4e3e1eac7e30106817e7a6b09cdadb41c9903
Gleb <62200674+alwayswanna@users.noreply.github.com>
2021-04-18 23:01:57 +0500

    + add POJO classes, add properties for connection to database;

---
+++ src/main/java/a/gleb/bus_station/Entity/Drivers.java
@@ -0,0 +1,82 @@
+package a.gleb.bus_station.Entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "drivers")
+public class Drivers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "key_driver")
+    private Integer id;
+    @Column(name = "driver_name")
+    private String driverName;
+    @Column(name = "driver_surname")
+    private String driverSurname;
+    @Column(name = "driver_phone")
+    private String driverPhone;
+
+    @OneToMany(mappedBy = "drivers")
+    private List<BusFlights> busFlights;
+
+    public Drivers(String driverName, String driverSurname, String driverPhone) {
+        this.driverName = driverName;
+        this.driverSurname = driverSurname;
+        this.driverPhone = driverPhone;
+    }
+
+    public Drivers() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getDriverSurname() {
+        return driverSurname;
+    }
+
+    public void setDriverSurname(String driverSurname) {
+        this.driverSurname = driverSurname;
+    }
+
+    public String getDriverPhone() {
+        return driverPhone;
+    }
+
+    public void setDriverPhone(String driverPhone) {
+        this.driverPhone = driverPhone;
+    }
+
+    public List<BusFlights> getBusFlights() {
+        return busFlights;
+    }
+
+    public void setBusFlights(List<BusFlights> busFlights) {
+        this.busFlights = busFlights;
+    }
+
+    public void addRouteToList(BusFlights busFlight){
+        if (busFlights == null){
+            busFlights = new ArrayList<>();
+        }else{
+            busFlights.add(busFlight);
+        }
+
+    }
+}
---
+++ src/main/java/a/gleb/bus_station/Entity/Ticket.java
@@ -0,0 +1,62 @@
+package a.gleb.bus_station.Entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ticket")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "num_ticket")
+    private Integer id;
+    @Column(name = "ticket_place")
+    private String ticketPlace;
+    @Column(name = "ticket_passenger")
+    private String ticketPassenger;
+
+    @OneToOne
+    @JoinColumn(name = "passenger_id")
+    private Passengers passengers;
+
+    public Ticket(String ticketPlace, String ticketPassenger) {
+        this.ticketPlace = ticketPlace;
+        this.ticketPassenger = ticketPassenger;
+    }
+
+    public Ticket() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTicketPlace() {
+        return ticketPlace;
+    }
+
+    public void setTicketPlace(String ticketPlace) {
+        this.ticketPlace = ticketPlace;
+    }
+
+    public String getTicketPassenger() {
+        return ticketPassenger;
+    }
+
+    public void setTicketPassenger(String ticketPassenger) {
+        this.ticketPassenger = ticketPassenger;
+    }
+
+    public Passengers getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(Passengers passengers) {
+        this.passengers = passengers;
+    }
+}
