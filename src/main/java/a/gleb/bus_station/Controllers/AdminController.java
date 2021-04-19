e82e3c2ff339c71c645c8b5931601cf2140daea4
Gleb <62200674+alwayswanna@users.noreply.github.com>
2021-04-19 23:07:13 +0500

    + add methods for Drivers, TypeBus, TypeFlights;

--- src/main/java/a/gleb/bus_station/Controllers/AdminController.java
+++ src/main/java/a/gleb/bus_station/Controllers/AdminController.java
@@ -1,8 +1,13 @@
 package a.gleb.bus_station.Controllers;
 
 import a.gleb.bus_station.Entity.BusFlights;
+import a.gleb.bus_station.Entity.Drivers;
+import a.gleb.bus_station.Entity.TypeBus;
+import a.gleb.bus_station.Entity.TypeFlight;
+import a.gleb.bus_station.Repositories.DriversRepo;
 import a.gleb.bus_station.Repositories.FlightRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import a.gleb.bus_station.Repositories.TypeBusRepo;
+import a.gleb.bus_station.Repositories.TypeFlightsRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,8 +19,69 @@ import java.util.Map;
 @RequestMapping("/administrations")
 public class AdminController {
 
-    @Autowired
-    FlightRepo flightRepo;
+
+    private final FlightRepo flightRepo;
+    private final DriversRepo driversRepo;
+    private final TypeBusRepo typeBusRepo;
+    private final TypeFlightsRepo typeFlightsRepo;
+
+    public AdminController(FlightRepo flightRepo, DriversRepo driversRepo,
+                           TypeBusRepo typeBusRepo, TypeFlightsRepo typeFlightsRepo) {
+        this.typeBusRepo = typeBusRepo;
+        this.flightRepo = flightRepo;
+        this.driversRepo = driversRepo;
+        this.typeFlightsRepo = typeFlightsRepo;
+    }
+
+    @RequestMapping(value = "/administrator")
+    public String administratorPage(Map<String, Object> model){
+        return "administrator";
+    }
+
+    @RequestMapping(value = "/add_typeFlight", method = RequestMethod.GET)
+    public String adminAddTypeFlightGet(Map<String, Object> model){
+        return "addTypeFlight";
+    }
+
+    @RequestMapping(value = "/add_typeFlight", method = RequestMethod.POST)
+    public String adminAddTypeFlightPost(@RequestParam String type
+                                        ,Map<String, Object> model){
+        TypeFlight typeFlight = new TypeFlight(type);
+        typeFlightsRepo.save(typeFlight);
+    return "redirect:/administrations/administrator";
+    }
+
+    @RequestMapping(value = "/add_typeBus", method = RequestMethod.GET)
+    public String adminAddTypeBusGet(Map<String, Object> model){
+        return "addTypeBus";
+    }
+
+    @RequestMapping(value = "/add_typeBus", method = RequestMethod.POST)
+    public String adminAddTypeBusPost(@RequestParam String type,
+                                  @RequestParam Integer numberOfSeats,
+                                  @RequestParam String busModel,
+                                  Map<String, Object> model){
+        TypeBus bus = new TypeBus(type, numberOfSeats, busModel);
+        typeBusRepo.save(bus);
+    return "redirect:/administrations/administrator";
+    }
+
+
+    @RequestMapping(value = "/add_driver", method = RequestMethod.GET)
+    public String adminAddDriverGet(Map<String, Object> model){
+        return "addDriver";
+    }
+
+    @RequestMapping(value = "/add_driver", method = RequestMethod.POST)
+    public String adminAddDriverPost(@RequestParam String driverName,
+                                     @RequestParam String driverSurname,
+                                     @RequestParam String driverPhone,
+                                     Map<String, Object> model){
+        Drivers driver = new Drivers(driverName, driverSurname, driverPhone);
+        driversRepo.save(driver);
+    return "redirect:/administrations/administrator";
+    }
+
 
     @RequestMapping(value = "/add_flight", method = RequestMethod.GET)
     public String adminAddFlightGet(Map<String, Object> model){
@@ -33,6 +99,6 @@ public class AdminController {
         BusFlights busFlight = new BusFlights(routeType, fromCity, toCity,
                 timeDeparture, timeArrival);
         flightRepo.save(busFlight);
-    return "redirect:/";
+    return "redirect:/administrations/administrator";
     }
 }
--- src/main/java/a/gleb/bus_station/Repositories/TypeFlights.java
+++
@@ -1,8 +0,0 @@
-package a.gleb.bus_station.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TypeFlights extends CrudRepository<TypeFlights, Integer> {
-
-    Iterable<TypeFlights> findAllById(int i);
-}
