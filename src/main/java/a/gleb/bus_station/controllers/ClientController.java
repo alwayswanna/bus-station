62b6c94388dae3c044d943f43921cbb197ffd1ec
Gleb <62200674+alwayswanna@users.noreply.github.com>
2021-04-23 20:58:15 +0500

    + add full logic for buyTicket such as client;

--- src/main/java/a/gleb/bus_station/controllers/ClientController.java
+++ src/main/java/a/gleb/bus_station/controllers/ClientController.java
@@ -1,24 +1,40 @@
 package a.gleb.bus_station.controllers;
 
 import a.gleb.bus_station.dto.BusFlights;
+import a.gleb.bus_station.dto.PassengerPassport;
+import a.gleb.bus_station.dto.Passengers;
+import a.gleb.bus_station.dto.Ticket;
 import a.gleb.bus_station.repositories.FlightRepo;
+import a.gleb.bus_station.repositories.PassengerPassportRepo;
+import a.gleb.bus_station.repositories.PassengersRepo;
+import a.gleb.bus_station.repositories.TicketRepo;
 import a.gleb.bus_station.service.SystemMethods;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 public class ClientController {
 
     private final FlightRepo flightRepo;
+    private final TicketRepo ticketRepo;
+    private final PassengerPassportRepo passengerPassportRepo;
+    private final PassengersRepo passengersRepo;
 
-    public ClientController(FlightRepo flightRepo) {
+    public ClientController(FlightRepo flightRepo, TicketRepo ticketRepo,
+                            PassengerPassportRepo passengerPassportRepo, PassengersRepo passengersRepo) {
         this.flightRepo = flightRepo;
+        this.ticketRepo = ticketRepo;
+        this.passengerPassportRepo = passengerPassportRepo;
+        this.passengersRepo = passengersRepo;
     }
 
     @RequestMapping(value = "/flight/{id}/buy_ticket", method = RequestMethod.GET)
@@ -32,6 +48,42 @@ public class ClientController {
             model.put("flight", busFlightsModel);
         }
         return "buyTicket";
+    }
+
+    @RequestMapping(value = "/flight/{id}/buy_ticket", method = RequestMethod.POST)
+    public String buyTicketPost(@PathVariable(value = "id") Integer id,
+                                @RequestParam String passengerName,
+                                @RequestParam String passengerSurname,
+                                @RequestParam String passengerPhone,
+                                @RequestParam String passengerDocNum,
+                                @RequestParam String passengerRegistration,
+                                @RequestParam String passengerBirthday,
+                                Map<String, Object> model){
+        Optional<BusFlights> busFlights = flightRepo.findById(id);
+        ArrayList<BusFlights> busFlightsArrayList = new ArrayList<>();
+        busFlights.ifPresent(busFlightsArrayList::add);
+        BusFlights busFlight = busFlightsArrayList.get(0);
+        model.put("flight", busFlight);
+
+
+
+        String uuid = UUID.randomUUID().toString();
+        String numTicket = busFlight.getNumberFlightUnique() + "_" + uuid.substring(0,4);
+        PassengerPassport passengerPassport = new PassengerPassport(passengerName, passengerSurname,
+                passengerPhone, passengerDocNum, passengerRegistration, passengerBirthday);
+                    Passengers passenger = new Passengers();
+                    passenger.setPassengerInfo(passengerPassport);
+                    passenger.setNumTicket(numTicket);
+        Ticket ticket = new Ticket();
+        ticket.setPassengers(passenger);
+        ticket.setTicketPassenger(passengerSurname);
+        ticket.setTicketPlace("Сидячее");
+        ticket.setBusFlights(busFlight);
+        passengerPassportRepo.save(passengerPassport);
+        passengersRepo.save(passenger);
+        ticketRepo.save(ticket);
+
+    return "redirect:/";
     }
 
 
