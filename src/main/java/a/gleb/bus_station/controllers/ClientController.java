3e60a2dd984c3f03508579ccef72c3f70813702b
Gleb <62200674+alwayswanna@users.noreply.github.com>
2021-04-23 21:30:28 +0500

    + add check for fields;

--- src/main/java/a/gleb/bus_station/controllers/ClientController.java
+++ src/main/java/a/gleb/bus_station/controllers/ClientController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -58,32 +59,49 @@ public class ClientController {
                                 @RequestParam String passengerDocNum,
                                 @RequestParam String passengerRegistration,
                                 @RequestParam String passengerBirthday,
-                                Map<String, Object> model){
-        Optional<BusFlights> busFlights = flightRepo.findById(id);
-        ArrayList<BusFlights> busFlightsArrayList = new ArrayList<>();
-        busFlights.ifPresent(busFlightsArrayList::add);
-        BusFlights busFlight = busFlightsArrayList.get(0);
-        model.put("flight", busFlight);
+                                Map<String, Object> model,
+                                RedirectAttributes redirectAttributes){
+        String redirect = "/flight/"+ id + "/buy_ticket";
+        if (passengerName.equals("") | passengerSurname.equals("") | passengerBirthday.equals("") |
+                passengerPhone.equals("") | passengerDocNum.equals("")| passengerBirthday.equals("")){
+            String errorStr = "Вы заполнили не все поля! Обновите страницу";
+            redirectAttributes.addFlashAttribute("error", errorStr);
+            return "redirect:" + redirect;
+        }else {
+
+            Optional<BusFlights> busFlights = flightRepo.findById(id);
+            ArrayList<BusFlights> busFlightsArrayList = new ArrayList<>();
+            busFlights.ifPresent(busFlightsArrayList::add);
+            BusFlights busFlight = busFlightsArrayList.get(0);
+            model.put("flight", busFlight);
+
+            String uuid = UUID.randomUUID().toString();
+            String numTicket = busFlight.getNumberFlightUnique() + "_" + uuid.substring(0, 4);
+            PassengerPassport passengerPassport = new PassengerPassport(passengerName, passengerSurname,
+                    passengerPhone, passengerDocNum, passengerRegistration, passengerBirthday);
+            Passengers passenger = new Passengers();
+            passenger.setPassengerInfo(passengerPassport);
+            passenger.setNumTicket(numTicket);
+            Ticket ticket = new Ticket();
+            ticket.setPassengers(passenger);
+            ticket.setTicketPassenger(passengerSurname);
+            ticket.setTicketPlace("Сидячее");
+            ticket.setBusFlights(busFlight);
+            passengerPassportRepo.save(passengerPassport);
+            passengersRepo.save(passenger);
+            ticketRepo.save(ticket);
+            String callBack = "Билет успешно приобретен." +
+                    "\n Сверьте данные, в случае неточности обратитесь в службу техничесой поддержки автовокзала:" +
+                    "\n Фамилия пассажира - " + passengerSurname +
+                    "\n Имя пассажира - " + passengerName +
+                    "\n Номер пасспорта пассажира - " + passengerDocNum +
+                    "\n Номер билета пассажира - " + numTicket +
+                    " Обновите страницу если хотите приобрести еще!";
+            redirectAttributes.addFlashAttribute("message", callBack);
+        }
 
 
-
-        String uuid = UUID.randomUUID().toString();
-        String numTicket = busFlight.getNumberFlightUnique() + "_" + uuid.substring(0,4);
-        PassengerPassport passengerPassport = new PassengerPassport(passengerName, passengerSurname,
-                passengerPhone, passengerDocNum, passengerRegistration, passengerBirthday);
-                    Passengers passenger = new Passengers();
-                    passenger.setPassengerInfo(passengerPassport);
-                    passenger.setNumTicket(numTicket);
-        Ticket ticket = new Ticket();
-        ticket.setPassengers(passenger);
-        ticket.setTicketPassenger(passengerSurname);
-        ticket.setTicketPlace("Сидячее");
-        ticket.setBusFlights(busFlight);
-        passengerPassportRepo.save(passengerPassport);
-        passengersRepo.save(passenger);
-        ticketRepo.save(ticket);
-
-    return "redirect:/";
+    return "redirect:" + redirect;
     }
 
 
