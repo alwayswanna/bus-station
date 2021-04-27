18bca6a23de1fd35eb95146aceed175d825deef1
Gleb <62200674+alwayswanna@users.noreply.github.com>
2021-04-27 21:37:54 +0500

    + add buy ticket with styles;

--- src/main/java/a/gleb/bus_station/controllers/PassengerAdminController.java
+++ src/main/java/a/gleb/bus_station/controllers/PassengerAdminController.java
@@ -7,11 +7,13 @@ import a.gleb.bus_station.dto.Ticket;
 import a.gleb.bus_station.repositories.FlightRepo;
 import a.gleb.bus_station.repositories.PassengerPassportRepo;
 import a.gleb.bus_station.repositories.PassengersRepo;
+import a.gleb.bus_station.repositories.TicketRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
 import java.util.UUID;
@@ -23,11 +25,14 @@ public class PassengerAdminController {
     private final PassengerPassportRepo passengerPassportRepo;
     private final PassengersRepo passengersRepo;
     private final FlightRepo flightRepo;
+    private final TicketRepo ticketRepo;
 
-    public PassengerAdminController(PassengerPassportRepo passengerPassportRepo, PassengersRepo passengersRepo, FlightRepo flightRepo) {
+    public PassengerAdminController(PassengerPassportRepo passengerPassportRepo, PassengersRepo passengersRepo,
+                                    FlightRepo flightRepo, TicketRepo ticketRepo) {
         this.passengerPassportRepo = passengerPassportRepo;
         this.passengersRepo = passengersRepo;
         this.flightRepo = flightRepo;
+        this.ticketRepo = ticketRepo;
     }
 
     @RequestMapping(value = "/administrator/passengers", method = RequestMethod.GET)
@@ -93,5 +98,59 @@ public class PassengerAdminController {
         passengerPassport.setPassengerRegistration(passengerRegistration);
         passengerPassportRepo.save(passengerPassport);
         return "redirect:/administrations/administrator/passengers";
+    }
+
+    @RequestMapping(value = "/administrator/buy_ticket", method = RequestMethod.GET)
+    public String administratorBuyTicketGet(Map<String, Object> model){
+        Iterable<BusFlights> flights = flightRepo.findAll();
+        model.put("flights", flights);
+        return "administrationBuyTicket";
+    }
+
+    @RequestMapping(value = "/administrator/buy_ticket", method = RequestMethod.POST)
+    public String administratorBuyTicketPost(@RequestParam String passengerName,
+                                             @RequestParam String passengerSurname,
+                                             @RequestParam String passengerPhone,
+                                             @RequestParam String passengerDocNum,
+                                             @RequestParam String passengerRegistration,
+                                             @RequestParam String passengerBirthday,
+                                             @RequestParam String numberFlightUnique,
+                                             Map<String, Object> model,
+                                             RedirectAttributes redirectAttributes){
+        String uuid = UUID.randomUUID().toString();
+        String redirectLink = "/administrations/administrator/buy_ticket";
+        if (passengerName.equals("") | passengerSurname.equals("") | passengerBirthday.equals("") |
+                passengerPhone.equals("") | passengerDocNum.equals("") | passengerBirthday.equals("")) {
+            String errorStr = "Вы заполнили не все поля! Обновите страницу";
+            redirectAttributes.addFlashAttribute("error", errorStr);
+            return "redirect:" + redirectLink;
+        } else {
+            BusFlights flight = flightRepo.findByNumberFlightUnique(numberFlightUnique);
+            PassengerPassport passengerPassport = new PassengerPassport(passengerName, passengerSurname,
+                    passengerPhone, passengerDocNum, passengerRegistration, passengerBirthday);
+            Passengers passenger = new Passengers();
+            passenger.setPassengerInfo(passengerPassport);
+            passenger.setNumTicket(
+                    flight.getNumberFlightUnique() + "_" + uuid.substring(0,4)
+            );
+            Ticket ticket = new Ticket();
+            ticket.setTicketPlace("Сидячее");
+            ticket.setTicketPassenger(passengerSurname);
+            ticket.setPassengers(passenger);
+            ticket.setBusFlights(flight);
+            passengerPassportRepo.save(passengerPassport);
+            passengersRepo.save(passenger);
+            ticketRepo.save(ticket);
+            String callBack = "Билет успешно приобретен." +
+                    "\n Сверьте данные, в случае неточности обратитесь в службу техничесой поддержки автовокзала:" +
+                    "\n Фамилия пассажира - " + passengerSurname +
+                    "\n Имя пассажира - " + passengerName +
+                    "\n Номер пасспорта пассажира - " + passengerDocNum +
+                    "\n Номер билета пассажира - " + flight.getNumberFlightUnique() + "_" + uuid.substring(0,4) +
+                    " Обновите страницу если хотите приобрести еще!";
+            redirectAttributes.addFlashAttribute("message", callBack);
+        }
+
+        return "redirect:" + redirectLink;
     }
 }
