a3eab35a0792385c48e9c1cd6c85c31846c9035a
Anon <a.gleb@yahoo.com>
2021-04-28 19:57:55 +0500

    + add styles for buses admin full;

---
+++ src/main/java/a/gleb/bus_station/controllers/TypeFlightAdminController.java
@@ -0,0 +1,46 @@
+package a.gleb.bus_station.controllers;
+
+
+import a.gleb.bus_station.dto.BusFlights;
+import a.gleb.bus_station.dto.TypeFlight;
+import a.gleb.bus_station.repositories.FlightRepo;
+import a.gleb.bus_station.repositories.TypeFlightsRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/administrations")
+public class TypeFlightAdminController {
+
+    private final TypeFlightsRepo typeFlightsRepo;
+    private final FlightRepo flightRepo;
+
+    public TypeFlightAdminController(TypeFlightsRepo typeFlightsRepo, FlightRepo flightRepo) {
+        this.typeFlightsRepo = typeFlightsRepo;
+        this.flightRepo = flightRepo;
+    }
+
+    @RequestMapping(value = "/administrator/type_flights", method = RequestMethod.GET)
+    public String administratorTypeFlightGet(Map<String, Object> model){
+        Iterable<BusFlights> flights =  flightRepo.findAll();
+        model.put("type_flight", flights);
+        return "administrationTypeFlight";
+    }
+
+    @RequestMapping(value = "/administrator/type_flight/arrival", method = RequestMethod.GET)
+    public String administratorArrivalFlightGet(Map<String, Object> model){
+        Iterable<BusFlights> flightsArrival = flightRepo.findAllByRouteType("Прибывающий");
+        model.put("type_flight", flightsArrival);
+        return "administrationTypeFlight";
+    }
+
+    @RequestMapping(value = "/administrator/type_flight/departure", method = RequestMethod.GET)
+    public String administratorDepartureFlightGet(Map<String, Object> model){
+        Iterable<BusFlights> flightsArrival = flightRepo.findAllByRouteType("Отбывающий");
+        model.put("type_flight", flightsArrival);
+        return "administrationTypeFlight";
+    }
+}
