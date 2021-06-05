0d061f5628588b2859cceba68c469d9ff6f7fc8e
agleb <a.gleb@yahoo.com>
2021-06-05 09:21:29 -0700

    + refactor controllers

--- src/main/java/a/gleb/bus_station/controllers/DriverAdminController.java
+++ src/main/java/a/gleb/bus_station/controllers/DriverAdminController.java
@@ -2,7 +2,8 @@ package a.gleb.bus_station.controllers;
 
 import a.gleb.bus_station.dto.BusDriver;
 import a.gleb.bus_station.dto.BusFlights;
-import a.gleb.bus_station.repositories.DriversRepo;
+import a.gleb.bus_station.service.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,30 +12,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
 import java.util.Map;
-import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/administrations")
 @PreAuthorize("hasAnyAuthority('OPERATOR', 'ADMINISTRATOR')")
 public class DriverAdminController {
 
-    private final DriversRepo driversRepo;
+    private final DriverService driverService;
 
-    public DriverAdminController(DriversRepo driversRepo) {
-        this.driversRepo = driversRepo;
+    @Autowired
+    public DriverAdminController(DriverService driverService) {
+        this.driverService = driverService;
     }
 
     @RequestMapping(value = "/administrator/drivers", method = RequestMethod.GET)
     @PreAuthorize("hasAnyAuthority('OPERATOR', 'ADMINISTRATOR')")
     public String administratorPageDrivers(Map<String, Object> model) {
-        Iterable<BusDriver> drivers = driversRepo.findAll();
+        Iterable<BusDriver> drivers = driverService.getAllDrivers();
         model.put("drivers", drivers);
         return "administratorDrivers";
     }
 
-    /*@RequestMapping(value = "/administrator/drivers/add_driver", method = RequestMethod.GET)
+    @RequestMapping(value = "/administrator/drivers/add_driver", method = RequestMethod.GET)
     @PreAuthorize("hasAnyAuthority('OPERATOR', 'ADMINISTRATOR')")
     public String adminAddDriverGet(Map<String, Object> model) {
         return "administrationAddDriver";
@@ -45,29 +45,27 @@ public class DriverAdminController {
     public String adminAddDriverPost(@RequestParam String driverName,
                                      @RequestParam String driverSurname,
                                      @RequestParam String driverPhone,
+                                     @RequestParam String driverLicence,
                                      Map<String, Object> model,
                                      RedirectAttributes redirectAttributes) {
-        if (driverName.equals("") | driverSurname.equals("") | driverPhone.equals("")) {
+        if (driverName.equals("") | driverSurname.equals("") | driverPhone.equals("") | driverLicence.equals("")) {
             String errorMsg = "Вы заполнили не все поля!";
             redirectAttributes.addFlashAttribute("error", errorMsg);
             return "redirect:/administrations/administrator/drivers/add_driver";
         } else {
-            BusDriver driver = new BusDriver(driverName, driverSurname, driverPhone, null);
-            driversRepo.save(driver);
+            driverService.addNewDriver(new BusDriver(driverName, driverSurname, driverPhone, driverLicence, null));
             return "redirect:/administrations/administrator/drivers";
         }
-    }*/
+    }
 
     @RequestMapping(value = "/administrator/drivers/{id}/edit", method = RequestMethod.GET)
     @PreAuthorize("hasAnyAuthority('OPERATOR', 'ADMINISTRATOR')")
     public String administratorDriverEditGet(@PathVariable(value = "id") Integer id,
                                              Map<String, Object> model) {
-        Optional<BusDriver> driver = driversRepo.findById(id);
-        ArrayList<BusDriver> driverModel = new ArrayList<>();
-        driver.ifPresent(driverModel::add);
-        Iterable<BusFlights> flights = driverModel.get(0).getBusFlights();
+        BusDriver driver = driverService.getSelectedDriver(id);
+        Iterable<BusFlights> flights = driver.getBusFlights();
         model.put("flights", flights);
-        model.put("driver", driverModel);
+        model.put("driver", driver);
         return "administrationBusOrDriverEdit";
     }
 
@@ -77,19 +75,15 @@ public class DriverAdminController {
                                               @RequestParam String driverName,
                                               @RequestParam String driverSurname,
                                               @RequestParam String driverPhone,
+                                              @RequestParam String driverLicense,
                                               Map<String, Object> model,
                                               RedirectAttributes redirectAttributes) {
-        if (driverName.equals("") | driverSurname.equals("") | driverPhone.equals("")) {
+        if (driverName.equals("") | driverSurname.equals("") | driverPhone.equals("") | driverLicense.equals("")) {
             String errorMsg = "Вы заполнили не все поля!";
             redirectAttributes.addFlashAttribute("error", errorMsg);
             return "redirect:/administrations/administrator/drivers/" + id + "/edit";
         } else {
-            int driverId = id;
-            BusDriver driver = driversRepo.findById(driverId);
-            driver.setDriverName(driverName);
-            driver.setDriverSurname(driverSurname);
-            driver.setDriverPhone(driverPhone);
-            driversRepo.save(driver);
+            driverService.editSelectedDriver(new BusDriver(driverName, driverSurname, driverPhone, driverLicense, driverService.getSelectedDriver(id).getBusFlights()));
             return "redirect:/administrations/administrator/drivers";
         }
     }
@@ -98,13 +92,7 @@ public class DriverAdminController {
     @PreAuthorize("hasAnyAuthority('OPERATOR', 'ADMINISTRATOR')")
     public String administratorDriverDelete(@PathVariable(value = "id") Integer id,
                                             Map<String, Object> model) {
-        int driverId = id;
-        BusDriver driver = driversRepo.findById(driverId);
-        Iterable<BusFlights> flights = driver.getBusFlights();
-        for (BusFlights flight : flights) {
-            flight.setBusDriver(driversRepo.findById(1));
-        }
-        driversRepo.delete(driver);
+        var res = driverService.deleteSelectedDriver(id);
         return "redirect:/administrations/administrator/drivers";
     }
 
@@ -112,13 +100,10 @@ public class DriverAdminController {
     @PreAuthorize("hasAnyAuthority('OPERATOR', 'ADMINISTRATOR')")
     public String administratorDriverGetFlightsGet(@PathVariable(value = "id") Integer id,
                                                    Map<String, Object> model) {
-        int driverId = id;
-        BusDriver busDriver = driversRepo.findById(driverId);
-        Iterable<BusFlights> flights = busDriver.getBusFlights();
-        model.put("driver", busDriver);
+        BusDriver driver = driverService.getSelectedDriver(id);
+        Iterable<BusFlights> flights = driver.getBusFlights();
+        model.put("driver", driver);
         model.put("flights", flights);
         return "administratorDriverFlights";
-
     }
-
 }
