bc28c98049e0fe7c7d4107091cac36c1d43f3f3d
agleb <a.gleb@yahoo.com>
2021-06-09 10:00:48 -0700

    + fix controller for user;

--- src/main/java/a/gleb/bus_station/controllers/MainController.java
+++ src/main/java/a/gleb/bus_station/controllers/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Controller
 public class MainController {
@@ -22,29 +23,45 @@ public class MainController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String indexPage(Map<String, Object> model){
-        Iterable<BusFlights> busFlights = flightService.allFlights();
-        model.put("flights",busFlights);
+        try{
+            Iterable<BusFlights> busFlights = flightService.allFlights();
+            model.put("flights",busFlights);
+        }catch (NoSuchElementException noSuchElementException){
+            model.put("flights", null);
+        }
         return "index";
     }
 
     @RequestMapping(value = "/flight/{id}", method = RequestMethod.GET)
     public String aboutFlight(@PathVariable(value = "id") Integer id, Map<String, Object> model){
-        BusFlights flight = flightService.returnFlightById(id);
-        model.put("flight", flight);
+        try{
+            BusFlights flight = flightService.returnFlightById(id);
+            model.put("flight", flight);
+        }catch (NoSuchElementException noSuchElementException){
+            model.put("flight", null);
+        }
         return "aboutFlight";
     }
 
     @RequestMapping(value = "/flights/arrival", method = RequestMethod.GET)
     public String getAllArrivalFlights(Map<String, Object> model){
-        Iterable<BusFlights> flights = flightService.flightsByType("Прибывающий");
-        model.put("flights", flights);
+        try{
+            Iterable<BusFlights> flights = flightService.flightsByType("Прибывающий");
+            model.put("flights", flights);
+        }catch (NoSuchElementException noSuchElementException){
+            model.put("flights", null);
+        }
         return "clientAnyFlights";
     }
 
     @RequestMapping(value = "/flights/departure", method = RequestMethod.GET)
     public String getAllDepartureFlights(Map<String, Object> model){
-        Iterable<BusFlights> flights = flightService.flightsByType("Отбывающий");
-        model.put("flights", flights);
+        try{
+            Iterable<BusFlights> flights = flightService.flightsByType("Отбывающий");
+            model.put("flights", flights);
+        }catch (NoSuchElementException noSuchElementException){
+            model.put("flights", null);
+        }
         return "clientAnyFlights";
     }
 
